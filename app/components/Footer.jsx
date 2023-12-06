e62bd4b56206cb2ead46360165fb1524572258f7
Jay-Karia <jay.sanjay.karia@gmail.com>
2023-12-06 21:07:49 +0530

    Add Footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,7 +1,14 @@
+import {Link} from "@nextui-org/react";
+
 function Footer() {
     return (
         <div className={"absolute border-2 p-3 bottom-0 w-full"}>
-            Footer
+                <h3 className="text-md float-right">
+                    developed by{' '}
+                    <Link underline={"hover"} target={"_blank"} href="https://github.com/Jay-Karia" className="text-md">
+                        Jay Karia
+                    </Link>
+                </h3>
         </div>
     );
 }
